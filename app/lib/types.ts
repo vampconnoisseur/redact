3b0ccc9d1edf9efc94e67a11fb6dd6ba8c4438bd
Jaiditya Batra <vampconnoisseur@gmail.com>
2025-08-28 14:04:38 +0530

    fix: bugs

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,5 +1,8 @@
 import { Prisma } from "@prisma/client";
 
+// By adding this comment, you are telling the linter that you intentionally
+// are not using this variable as a value, and it should not trigger a warning.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const userWithDocuments = Prisma.validator<Prisma.UserDefaultArgs>()({
     include: {
         ownedDocuments: true,
